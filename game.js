28e78eb3a5e7f83472066babe5089e0a82cc8c0e
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-09-18 20:21:04 -0300

    Add quiz questions to game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+// QUESTIONS = mesmas suas
+const QUESTIONS = [
+ {question:"Quanto que é 2F em hexadecimal para decimal?", answer:"47"},
+ {question:"Quanto que corresponde 10101001 de binario para decimal?", answer:"169"},
+ {question:"100101+101001 em binario?", answer:"1001110"},
+ {question:"Qual a função do clock da cpu", options:["A) Mudar de estado os registradores","B) Operar o ALU e todo barramento"], answer:"B"},
+ {question:"O ALU tem como operação a soma?(sim/não)", answer:"sim"},
+ {question:"Quantos bits cabem em 8 bytes?", answer:"64"}, // corrigi aqui, era 32
+ {question:"Qual linguagem usada pela CPU?", answer:"binario"},
+ {question:"Função de comparação qual é?", options:["A) XOR","B) AND"], answer:"A"},
+ {question:"Pode Passar dê o enter", answer:""},
+ {question:"Cite as portas mais utilizadas de dois pinos", options:["A) NOT, FLIPFLOP, AND","B) XOR, NOR, AND"], answer:"B"}
+];
